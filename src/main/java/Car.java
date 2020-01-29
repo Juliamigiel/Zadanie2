a5b5a47130dc0377bb11058bac903822cfc2e73a
Juliamigiel <Julia.migiel@gmail.com>
2020-01-29 23:08:35 +0100

    Car.class to Car

---
+++ src/main/java/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+
+    private String make;
+    private String registration;
+
+    public Car(String make, String registration) {
+        this.make = make;
+        this.registration = registration;
+    }
+    public void drivecarefull(){
+        System.out.println("Car is Carefull");
+    }
+}
